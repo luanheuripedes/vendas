701bf13aa735f345d5e7d8e3790ba7491243f2a5
luanheuripedes <luanheuripedesmoreiradepaula@gmail.com>
2023-08-16 19:24:11 -0300

    learning: componente View

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,22 @@
-import React, { useState } from 'react';
-import { SafeAreaView, Text, Button } from 'react-native';
+import React, { } from 'react';
+import { SafeAreaView, Text, Button, View, StyleSheet } from 'react-native';
 import { useApp } from './useApp';
 
 
+const styles = StyleSheet.create({
+  container: {
+    color: 'blue',
+  },
+  view1: {
+    display: 'flex',
+    backgroundColor: 'blue',
+    padding: 24,
+    flexDirection: 'row',
+  },
+  view2: {
+    backgroundColor: 'pink',
+  },
+});
 
 
 const App = () => {
@@ -10,6 +24,14 @@ const App = () => {
 
   return (
     <SafeAreaView>
+      <View style={styles.view1}>
+        <View style={styles.view2}>
+          <Text>Novo Teste</Text>
+        </View>
+        <View style={styles.view2}>
+          <Text>Novo Teste</Text>
+        </View>
+      </View>
       <Text>{value}</Text>
       <Button title="BOTAO" onPress={handleOnPress} />
     </SafeAreaView>

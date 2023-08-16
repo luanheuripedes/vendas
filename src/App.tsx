a5418373addbad452b33b7779cbf9ad53ae61521
luanheuripedes <luanheuripedesmoreiradepaula@gmail.com>
2023-08-15 21:19:38 -0300

    feat: insert styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
-import React  from 'react';
-import { SafeAreaView, Text } from 'react-native';
+import React from 'react';
+import { SafeAreaView, Text, StyleSheet } from 'react-native';
+import styled from 'styled-components/native';
 
+// styled components
+const TextNew = styled.Text`
+  font-size:  24px;
+  color: blue;
+`;
+
+const styles = StyleSheet.create({
+  container: {
+    color: 'blue',
+  },
+});
 
 const App = () => {
   return (
     <SafeAreaView>
-      <Text>Testando</Text>
+      <Text style={styles.container}>Testando</Text>
+      <TextNew>Novo Teste</TextNew>
     </SafeAreaView>
   );
 };

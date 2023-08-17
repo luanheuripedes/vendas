bf43a4fd647c789416951b28bb98ea6572f947e0
luanheuripedes <luanheuripedesmoreiradepaula@gmail.com>
2023-08-16 21:30:57 -0300

    feat: create modules login

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,15 @@
 import React, { } from 'react';
-import { FlatList, SafeAreaView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { SafeAreaView } from 'react-native';
+import Login from './modules/login';
 
-
-const styles = StyleSheet.create({
-  container: {
-    color: 'blue',
-    fontSize: 30,
-  },
-  flat: {
-    backgroundColor: '#ddd',
-  },
-  input: {
-    backgroundColor: '#ddd',
-    margin: 16,
-  },
-  scroll: {
-    backgroundColor: 'green',
-    height: 100,
-  },
-  touchable:{
-    backgroundColor: 'blue',
-    padding: 16,
-    borderRadius:4,
-    margin:16,
-  },
-});
 
 
 
 const App = () => {
-  const handleOnClick = () =>{
-console.log('Passou o click');
-  };
-
 
   return (
     <SafeAreaView>
-      <TouchableOpacity onPress={handleOnClick} style = {styles.touchable}>
-        <View>
-          <Text>Clicar</Text>
-        </View>
-      </TouchableOpacity>
-
+      <Login />
     </SafeAreaView>
   );
 };
